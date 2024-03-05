f2c8682f0656f4aee405cc44e8309bfab06ba4fd
Marek Zając <contact@zajacmarek.com>
2024-03-05 18:59:07 +0100

    Add unit test

--- src/Emu8086/Emu8086.Core.Tests/BiuTests.cs
+++ src/Emu8086/Emu8086.Core.Tests/BiuTests.cs
@@ -1,6 +1,23 @@
-﻿namespace Emu8086.Core.Tests;
+﻿using Emu8086.Core.MemoryAddresses;
+
+namespace Emu8086.Core.Tests;
 
 public class BiuTests
 {
-    
+    [Fact]
+    public void When_WriteToMemory_Expect_ValueIsWritten()
+    {
+        var memory = new Memory();
+        // Arrange
+        var biu = new Biu(memory, new SegmentRegisters());
+        var address = new DirectAddressing(0x1000);
+        var value = (ushort)0x1234;
+
+        // Act
+        biu.Write(address, value);
+        var result = biu.Read(address);
+
+        // Assert
+        Assert.Equal(value, result);
+    }
 }
---
+++ src/Emu8086/Emu8086.Core/MemoryAddresses/DirectAddressing.cs
@@ -0,0 +1,8 @@
+﻿using Emu8086.Core.Interfaces;
+
+namespace Emu8086.Core.MemoryAddresses;
+
+public class DirectAddressing(ushort address) : IMemoryAddress
+{
+    public uint Address => address;
+}
