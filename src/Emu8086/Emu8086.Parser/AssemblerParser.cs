ad96cea95162119d26610f9759f70266967a681a
Marek Zając <contact@zajacmarek.com>
2024-03-08 17:02:35 +0100

    Read code lines

---
+++ src/Emu8086/Emu8086.Parser/AssemblerParser.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Emu8086.Parser;
+
+public class AssemblerParser : IAssemblerParser
+{
+    public byte[] Parse(string code)
+    {
+        var codeStream = GetStream(code);
+
+        var bytes = new List<byte>();
+
+        while (codeStream.Position < codeStream.Length)
+        {
+            var line = GetNextLine(codeStream);
+            var instruction = ParseInstruction(line);
+            bytes.AddRange(instruction);
+        }
+
+        return bytes.ToArray();
+    }
+
+    private ReadOnlySpan<byte> ParseInstruction(string? line)
+    {
+        throw new NotImplementedException();
+    }
+
+    private MemoryStream GetStream(string code)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(code);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+
+    private string? GetNextLine(MemoryStream stream)
+    {
+        throw new NotImplementedException();
+    }
+}
